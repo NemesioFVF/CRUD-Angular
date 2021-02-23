a4d0011dbec70f9038cbe4926da345bffaf4618a
Vagner Treguas <main.smartlaw@gmail.com>
2021-02-23 17:52:52 -0300

    finalizado as funções basicas do projeto

--- frontend/src/app/components/student/student.service.ts
+++ frontend/src/app/components/student/student.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { HttpClient } from '@angular/common/http';
 import { Student } from './student.model';
-import { Observable } from 'rxjs';
+import { Observable, EMPTY } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -12,34 +13,55 @@ export class StudentService {
 
   constructor(private snackBar: MatSnackBar, private http: HttpClient) {}
 
-  showMessage(msg: string): void {
+  showMessage(msg: string, isError: boolean = false): void {
     this.snackBar.open(msg, 'Fechar', {
       duration: 3000,
       horizontalPosition: 'right',
       verticalPosition: 'top',
+      panelClass: isError ? ['msg-error'] : ['msg-success'],
     });
   }
 
   create(student: Student): Observable<Student> {
-    return this.http.post<Student>(this.url, student);
+    return this.http.post<Student>(this.url, student).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
   }
 
   read(): Observable<Student[]> {
-    return this.http.get<Student[]>(this.url);
+    return this.http.get<Student[]>(this.url).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
   }
 
   readById(id: number): Observable<Student> {
     const url = `${this.url}/${id}`;
-    return this.http.get<Student>(url);
+    return this.http.get<Student>(url).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
   }
 
   update(student: Student): Observable<Student> {
     const url = `${this.url}/${student.id}`;
-    return this.http.put<Student>(url, student);
+    return this.http.put<Student>(url, student).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
   }
 
   delete(id: number): Observable<Student> {
     const url = `${this.url}/${id}`;
-    return this.http.delete<Student>(url);
+    return this.http.delete<Student>(url).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
+  }
+  //Mensagem de erro relacionado se o banco de dados estiver Off
+  errorHandler(e: any): Observable<any> {
+    this.showMessage('Ocorreu um erro', true);
+    return EMPTY;
   }
 }
